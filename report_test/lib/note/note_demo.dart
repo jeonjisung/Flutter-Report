f6796410db9c9fbee0d3aeb2d6c09b9529279a47
jeonjisung <ansfk87@naver.com>
2022-11-27 15:09:31 +0900

    2022.11.27

---
+++ report_test/lib/note/note_demo.dart
@@ -0,0 +1,281 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+_Note_DemoState pageState;
+
+class Note_Demo extends StatefulWidget {
+  const Note_Demo({Key key}) : super(key: key);
+
+  @override
+  _Note_DemoState createState() {
+    pageState = _Note_DemoState();
+    return pageState;
+  }
+}
+
+class _Note_DemoState extends State<Note_Demo> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  // 컬렉션명
+  final String colName = "FirstDemo";
+
+  // 필드명
+  final String fnName = "name";
+  final String fnDescription = "description";
+  final String fnDatetime = "datetime";
+
+  TextEditingController _newNameCon = TextEditingController();
+  TextEditingController _newDescCon = TextEditingController();
+  TextEditingController _undNameCon = TextEditingController();
+  TextEditingController _undDescCon = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(title: Text("FirestoreFirstDemo")),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 500,
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection(colName)
+                  .orderBy(fnDatetime, descending: true)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) return Text("Error: ${snapshot.error}");
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return Text("Loading...");
+                  default:
+                    return ListView(
+                      children: snapshot.data.documents
+                          .map((DocumentSnapshot document) {
+                        Timestamp ts = document[fnDatetime];
+                        String dt = timestampToStrDateTime(ts);
+                        return Card(
+                          elevation: 2,
+                          child: InkWell(
+                            // Read Document
+                            onTap: () {
+                              showDocument(document.documentID);
+                            },
+                            // Update or Delete Document
+                            onLongPress: () {
+                              showUpdateOrDeleteDocDialog(document);
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.all(8),
+                              child: Column(
+                                children: <Widget>[
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Text(
+                                        document[fnName],
+                                        style: TextStyle(
+                                          color: Colors.blueGrey,
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        dt.toString(),
+                                        style:
+                                            TextStyle(color: Colors.grey[600]),
+                                      ),
+                                    ],
+                                  ),
+                                  Container(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      document[fnDescription],
+                                      style: TextStyle(color: Colors.black54),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    );
+                }
+              },
+            ),
+          )
+        ],
+      ),
+      // Create Document
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add), onPressed: showCreateDocDialog),
+    );
+  }
+
+  /// Firestore CRUD Logic
+
+  // 문서 생성 (Create)
+  void createDoc(String name, String description) {
+    FirebaseFirestore.instance.collection(colName).add({
+      fnName: name,
+      fnDescription: description,
+      fnDatetime: Timestamp.now(),
+    });
+  }
+
+  // 문서 조회 (Read)
+  void showDocument(String documentID) {
+    FirebaseFirestore.instance
+        .collection(colName)
+        .document(documentID)
+        .get()
+        .then((doc) {
+      showReadDocSnackBar(doc);
+    });
+  }
+
+  // 문서 갱신 (Update)
+  void updateDoc(String docID, String name, String description) {
+    FirebaseFirestore.instance.collection(colName).document(docID).updateData({
+      fnName: name,
+      fnDescription: description,
+    });
+  }
+
+  // 문서 삭제 (Delete)
+  void deleteDoc(String docID) {
+    FirebaseFirestore.instance.collection(colName).document(docID).delete();
+  }
+
+  void showCreateDocDialog() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Create New Document"),
+          content: Container(
+            height: 200,
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(labelText: "Name"),
+                  controller: _newNameCon,
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: "Description"),
+                  controller: _newDescCon,
+                )
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                _newNameCon.clear();
+                _newDescCon.clear();
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text("Create"),
+              onPressed: () {
+                if (_newDescCon.text.isNotEmpty &&
+                    _newNameCon.text.isNotEmpty) {
+                  createDoc(_newNameCon.text, _newDescCon.text);
+                }
+                _newNameCon.clear();
+                _newDescCon.clear();
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void showReadDocSnackBar(DocumentSnapshot doc) {
+    _scaffoldKey.currentState
+      ..hideCurrentSnackBar()
+      ..showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.deepOrangeAccent,
+          duration: Duration(seconds: 5),
+          content: Text(
+              "$fnName: ${doc[fnName]}\n$fnDescription: ${doc[fnDescription]}"
+              "\n$fnDatetime: ${timestampToStrDateTime(doc[fnDatetime])}"),
+          action: SnackBarAction(
+            label: "Done",
+            textColor: Colors.white,
+            onPressed: () {},
+          ),
+        ),
+      );
+  }
+
+  void showUpdateOrDeleteDocDialog(DocumentSnapshot doc) {
+    _undNameCon.text = doc[fnName];
+    _undDescCon.text = doc[fnDescription];
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Update/Delete Document"),
+          content: Container(
+            height: 200,
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: "Name"),
+                  controller: _undNameCon,
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: "Description"),
+                  controller: _undDescCon,
+                )
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                _undNameCon.clear();
+                _undDescCon.clear();
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text("Update"),
+              onPressed: () {
+                if (_undNameCon.text.isNotEmpty &&
+                    _undDescCon.text.isNotEmpty) {
+                  updateDoc(doc.documentID, _undNameCon.text, _undDescCon.text);
+                }
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text("Delete"),
+              onPressed: () {
+                deleteDoc(doc.documentID);
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  String timestampToStrDateTime(Timestamp ts) {
+    return DateTime.fromMicrosecondsSinceEpoch(ts.microsecondsSinceEpoch)
+        .toString();
+  }
+}
